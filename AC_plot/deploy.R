7ffa26110f5b7c8fc10e0d26fecf48554eb1888e
Mengbing Li <limengbinggz@gmail.com>
2018-09-12 19:52:26 -0400

    fix last commit

--- AC_plot/deploy.R
+++
@@ -1,7 +0,0 @@
-library(rsconnect)
-
-rsconnect::setAccountInfo(name='mengbingli',
-                          token='E33A100270AB175C326B017AEE1BD29D',
-                          secret='RwDTTf46wHnWrM4UXCVcqiXc+ggwgKis1TTrbchl')
-deployApp("C:/Users/mengbing/Box Sync/Optum Insight - Data Management/AC_plot",launch.browser = FALSE)
-Y
--- analysis/test_codes/run_test.R
+++
@@ -1,2 +0,0 @@
-Sys.setenv(RSTUDIO_PANDOC="C:/Program Files/RStudio/bin/pandoc")
-rmarkdown::render('C:/Users/mengbing/Box Sync/OptumInsight_DataManagement/analysis/test_codes_logististicRegression.Rmd', clean=TRUE)
