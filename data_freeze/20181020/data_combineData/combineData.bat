9edde77048702180b5ace13fb02a641e30f190d3
Mengbing Li <limengbinggz@gmail.com>
2018-10-21 16:11:15 -0400

    modify combineData to wide

--- data_freeze/20181020/data_combineData/combineData.bat
+++ data_freeze/20181020/data_combineData/combineData.bat
@@ -1,2 +1,2 @@
-"C:\Program Files\R\R-3.4.1\bin\R.exe" CMD BATCH "C:\Users\mengbing\Box Sync\OptumInsight_DataManagement\data_freeze\20180830\data_combineData\combineData.R"
+"C:\Program Files\R\R-3.4.1\bin\x64\R.exe" CMD BATCH --no-save "C:\Users\mengbing\Box Sync\OptumInsight_DataManagement\data_freeze\20181020\data_combineData\combineData_wide.R"
 
